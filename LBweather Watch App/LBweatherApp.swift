bdcfcc2b14b77d29176b3156c855674d2c8cc12e
pkthor <pkthor@gmail.com>
2024-09-23 10:38:49 -0600

    Initial Commit

---
+++ LBweather Watch App/LBweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBweatherApp.swift
+//  LBweather Watch App
+//
+//  Created by P. Kurt Thorderson on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LBweather_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LBweather/LBweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBweatherApp.swift
+//  LBweather
+//
+//  Created by P. Kurt Thorderson on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LBweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
